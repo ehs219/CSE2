e1b5c4ac1e0834a46e321039c13734377dfcce3e
ehs219 <ehs219@lehigh.edu>
2016-02-24 03:34:51 +0000

    updated hw04

---
+++ ToHex2.java
@@ -0,0 +1,221 @@
+import java.util.Scanner;
+
+public class ToHex {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter three numbers representing RGB values: "); //asks the user for RGB values
+        int R = input.nextInt();
+        int G = input.nextInt();
+        int B = input.nextInt();
+        
+        String hexR = "";
+        String hexR2 = "";
+        String hexG = "";
+        String hexG2 = "";
+        String hexB = "";
+        String hexB2 = "";
+        
+        switch ((int) R/16) {
+            case 0:
+                hexR = 0;
+                break;
+            case 1:
+                hexR = 1;
+                break;
+            case 2:
+                hexR = 2;
+                break;
+            case 3:
+                hexR = 3;
+                break;
+            case 4:
+                hexR = 4;
+                break;
+            case 5:
+                hexR = "5";
+                break;
+            case 6:
+                hexR = "6";
+                break;
+            case 7:
+                hexR = "7";
+                break;
+            case 8:
+                hexR = "8";
+                break;
+            case 9:
+                hexR = "9";
+                break;
+            case 10:
+                hexR = "A";
+                break;
+            case 11:
+                hexR = "B";
+                break;
+            case 12:
+                hexR = "C";
+                break;
+            case 13:
+                hexR = "D";
+                break;
+            case 14:
+                hexR = "E";
+                break;
+            case 15:
+                hexR = "F";
+                break;
+        }
+        
+        switch ((int) G/16) {
+            case 0:
+                hexG = 0;
+                break;
+            case 1:
+                hexG = 1;
+                break;
+            case 2:
+                hexG = 2;
+                break;
+            case 3:
+                hexG = 3;
+                break;
+            case 4:
+                hexG = 4;
+                break;
+            case 5:
+                hexG = "5";
+                break;
+            case 6:
+                hexG = "6";
+                break;
+            case 7:
+                hexG = "7";
+                break;
+            case 8:
+                hexG = "8";
+                break;
+            case 9:
+                hexG = "9";
+                break;
+            case 10:
+                hexG = "A";
+                break;
+            case 11:
+                hexG = "B";
+                break;
+            case 12:
+                hexG = "C";
+                break;
+            case 13:
+                hexG = "D";
+                break;
+            case 14:
+                hexG = "E";
+                break;
+            case 15:
+                hexG = "F";
+                break;
+        }
+        
+        switch ((int) B/16) {
+            case 0:
+                hexB = 0;
+                break;
+            case 1:
+                hexB = 1;
+                break;
+            case 2:
+                hexB = 2;
+                break;
+            case 3:
+                hexB = 3;
+                break;
+            case 4:
+                hexB = 4;
+                break;
+            case 5:
+                hexB = "5";
+                break;
+            case 6:
+                hexB = "6";
+                break;
+            case 7:
+                hexB = "7";
+                break;
+            case 8:
+                hexB = "8";
+                break;
+            case 9:
+                hexB = "9";
+                break;
+            case 10:
+                hexB = "A";
+                break;
+            case 11:
+                hexB = "B";
+                break;
+            case 12:
+                hexB = "C";
+                break;
+            case 13:
+                hexB = "D";
+                break;
+            case 14:
+                hexB = "E";
+                break;
+            case 15:
+                hexB = "F";
+                break;
+        }
+        
+        switch (R%16) {
+            case 0:
+                hexR2 = "0";
+                break;
+            case 1:
+                hexR2 = "1";
+                break;
+            case 2:
+                hexR2 = "2";
+                break;
+            case 3:
+                hexR2 = "3";
+                break;
+            case 4:
+                hexR2 = "4";
+                break;
+            case 5:
+                hexR2 = "5";
+                break;
+            case 6:
+                hexR2 = "6";
+                break;
+            case 7:
+                hexR2 = "7";
+                break;
+            case 8:
+                hexR2 = "8";
+                break;
+            case 9:
+                hexR2 = "9";
+                break;
+            case 10:
+                hexR2 = "A";
+                break;
+            case 11:
+                hexR2 = "B";
+                break;
+            case 12:
+                hexR2 = "C";
+                break;
+            case 13:
+                hexR2 = "D";
+                break;
+            case 14:
+                hexR2 = "E";
+                break;
+            case 15:
+                hexR2 = "F";
+                break;
+        }
--- hw04/ToHex.java
+++ hw04/ToHex.java
@@ -1,177 +1,224 @@
+// This program uses three inputs as decimal numbers and converts them to hexadecimal.
+
 import java.util.Scanner;
 
 public class ToHex {
     public static void main(String[] args) {
         
-        Scanner hexadecimal = new Scanner(System.in);
-        System.out.print("Please enter three numbers representing RGB values. R: "); //asks the user for RGB values
-        int R = hexadecimal.nextInt();
-        System.out.print("G: ");
-        int G = hexadecimal.nextInt();
-        System.out.print("B: ");
-        int B = hexadecimal.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter three numbers representing RGB values: "); //asks the user for RGB values
         
-        String HexR = "";
-        String HexG = "";
-        String HexB = "";
-        int HexR2 = (int) R/16;
-        int HexG2 = (int) G/16;
-        int HexB2 = (int) B/16;
+        int R=0; //initializes values for R, G, and B
+        int G=0;
+        int B=0;
         
-        switch (R%16) {
-            case 0:
-                HexR = "0";
-                break;
-            case 1:
-                HexR = "1";
-                break;
-            case 2:
-                HexR = "2";
-                break;
-            case 3:
-                HexR = "3";
-                break;
-            case 4:
-                HexR = "4";
-                break;
-            case 5:
-                HexR = "5";
-                break;
-            case 6:
-                HexR = "6";
-                break;
-            case 7:
-                HexR = "7";
-                break;
-            case 8:
-                HexR = "8";
-                break;
-            case 9:
-                HexR = "9";
-                break;
-            case 10:
-                HexR = "A";
-                break;
-            case 11:
-                HexR = "B";
-                break;
-            case 12:
-                HexR = "C";
-                break;
-            case 13:
-                HexR = "D";
-                break;
-            case 14:
-                HexR = "E";
-                break;
-            case 15:
-                HexR = "F";
-                break;
+        if (input.hasNextInt()) { //checks that the inputs are integers
+            R = input.nextInt();
+            if (input.hasNextInt()) {
+                G = input.nextInt();
+                if (input.hasNextInt()) {
+                    B = input.nextInt();
+                }
+            }
+            if (R>=0 && R<=255 && G>=0 && G<=255 && B>=0 && B<=255) { //checks that the inputs are in the correct ranges
+            
+            char hexR = '0'; //initializes the variables that will be the numbers in hexadecimal
+            char hexG = '0';
+            char hexB = '0';
+            char hexR2 = '0';
+            char hexG2 = '0';
+            char hexB2 = '0';
+            
+            if ( ((int) R/16) >=10 ) { //finds one digit of the R number in hexadecimal
+                switch ((int) R/16) {
+                    case 10:
+                        hexR2 = 'A';
+                        break;
+                    case 11:
+                        hexR2 = 'B';
+                        break;
+                    case 12:
+                        hexR2 = 'C';
+                        break;
+                    case 13:
+                        hexR2 = 'E';
+                        break;
+                    case 14:
+                        hexR2 = 'E';
+                        break;
+                    case 15:
+                        hexR2 = 'F';
+                        break;
+                }
+            } 
+            else {
+                hexR2 = Integer.toString((int) R/16).charAt(0);
+            }
+            
+            if ( ((int) G/16) >=10 ) { //finds one digit of the G number in hexadecimal
+                switch ((int) G/16) {
+                    case 10:
+                        hexG2 = 'A';
+                        break;
+                    case 11:
+                        hexG2 = 'B';
+                        break;
+                    case 12:
+                        hexG2 = 'C';
+                        break;
+                    case 13:
+                        hexG2 = 'E';
+                        break;
+                    case 14:
+                        hexG2 = 'E';
+                        break;
+                    case 15:
+                        hexG2 = 'F';
+                        break;
+                }
+            } 
+            else {
+                hexG2 = Integer.toString((int) G/16).charAt(0);
+            }
+            
+            if ( ((int) B/16) >=10 ) { //finds one digit of the B number in hexadecimal
+                switch ((int) B/16) {
+                    case 10:
+                        hexB2 = 'A';
+                        break;
+                    case 11:
+                        hexB2 = 'B';
+                        break;
+                    case 12:
+                        hexB2 = 'C';
+                        break;
+                    case 13:
+                        hexB2 = 'E';
+                        break;
+                    case 14:
+                        hexB2 = 'E';
+                        break;
+                    case 15:
+                        hexB2 = 'F';
+                        break;
+                }
+            } 
+            else {
+                hexB2 = Integer.toString((int) B/16).charAt(0);
+            }
+    
+            if (R%16 > 9) { //finds the other digit of the R number in hexadecimal
+                switch (R%16) {
+                    case 10:
+                        hexR = 'A';
+                        break;
+                    case 11:
+                        hexR = 'B';
+                        break;
+                    case 12:
+                        hexR = 'C';
+                        break;
+                    case 13:
+                        hexR = 'D';
+                        break;
+                    case 14:
+                        hexR = 'E';
+                        break;
+                    case 15:
+                        hexR = 'F';
+                        break;
+                }
+            }
+            else {
+                hexR = Integer.toString((int)R%16).charAt(0);
+            }
+            
+            if (G%16 > 9) { //finds the other digit of the G number in hexadecimal
+                switch (G%16) {
+                    case 10:
+                        hexG = 'A';
+                        break;
+                    case 11:
+                        hexG = 'B';
+                        break;
+                    case 12:
+                        hexG = 'C';
+                        break;
+                    case 13:
+                        hexG = 'D';
+                        break;
+                    case 14:
+                        hexG = 'E';
+                        break;
+                    case 15:
+                        hexG = 'F';
+                        break;
+                }
+            }
+            else {
+                hexG = Integer.toString((int)G%16).charAt(0);
+            }
+            
+            switch (B%16) { //finds the other digit of the B number in hexadecimal
+                case 0:
+                    hexB = '0';
+                    break;
+                case 1:
+                    hexB = '1';
+                    break;
+                case 2:
+                    hexB = '2';
+                    break;
+                case 3:
+                    hexB = '3';
+                    break;
+                case 4:
+                    hexB = '4';
+                    break;
+                case 5:
+                    hexB = '5';
+                    break;
+                case 6:
+                    hexB = '6';
+                    break;
+                case 7:
+                    hexB = '7';
+                    break;
+                case 8:
+                    hexB = '8';
+                    break;
+                case 9:
+                    hexB = '9';
+                    break;
+                case 10:
+                    hexB = 'A';
+                    break;
+                case 11:
+                    hexB = 'B';
+                    break;
+                case 12:
+                    hexB = 'C';
+                    break;
+                case 13:
+                    hexB = 'D';
+                    break;
+                case 14:
+                    hexB = 'E';
+                    break;
+                case 15:
+                    hexB = 'F';
+                    break;
+            }
+            
+            System.out.println("The decimal numbers R: "+R+", G: "+G+", B: "+B+", is represented in hexadecimal as: "+hexR2+hexR+hexG2+hexG+hexB2+hexB);
+            }
+            else {
+                System.out.println("Sorry, you must enter values between 0 and 255");
+            }
+        } else {
+            System.out.println("Sorry, your input must consist of integers");
         }
         
-        switch (G%16) {
-            case 0:
-                HexG = "0";
-                break;
-            case 1:
-                HexG = "1";
-                break;
-            case 2:
-                HexG = "2";
-                break;
-            case 3:
-                HexG = "3";
-                break;
-            case 4:
-                HexG = "4";
-                break;
-            case 5:
-                HexG = "5";
-                break;
-            case 6:
-                HexG = "6";
-                break;
-            case 7:
-                HexG = "7";
-                break;
-            case 8:
-                HexG = "8";
-                break;
-            case 9:
-                HexG = "9";
-                break;
-            case 10:
-                HexG = "A";
-                break;
-            case 11:
-                HexG = "B";
-                break;
-            case 12:
-                HexG = "C";
-                break;
-            case 13:
-                HexG = "D";
-                break;
-            case 14:
-                HexG = "E";
-                break;
-            case 15:
-                HexG = "F";
-                break;
-        }
-        
-        switch (B%16) {
-            case 0:
-                HexB = "0";
-                break;
-            case 1:
-                HexB = "1";
-                break;
-            case 2:
-                HexB = "2";
-                break;
-            case 3:
-                HexB = "3";
-                break;
-            case 4:
-                HexB = "4";
-                break;
-            case 5:
-                HexB = "5";
-                break;
-            case 6:
-                HexB = "6";
-                break;
-            case 7:
-                HexB = "7";
-                break;
-            case 8:
-                HexB = "8";
-                break;
-            case 9:
-                HexB = "9";
-                break;
-            case 10:
-                HexB = "A";
-                break;
-            case 11:
-                HexB = "B";
-                break;
-            case 12:
-                HexB = "C";
-                break;
-            case 13:
-                HexB = "D";
-                break;
-            case 14:
-                HexB = "E";
-                break;
-            case 15:
-                HexB = "F";
-                break;
-        }
-        
-        System.out.println(HexR2+HexR+HexG2+HexG+HexB2+HexB);
-        
+
     }
 }
